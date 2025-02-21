66898e3e4bf821b8e3ae27c8ea2e46587c462315
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2025-02-21 23:18:17 +0530

    add all routes - AIO

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ export default function Navbar() {
             <h1 className="text-3xl text-amber-950"><Link href="/">ScriptAndScribe</Link></h1>
             <div className="">
                 <ul className="flex items-center gap-5 font-medium text-lg">
-                    <li className="px-3 rounded-full ease-in-out hover:-translate-y-1.5 hover:font-bold duration-500"><Link href="/store">Store</Link></li>
-                    <li className="px-3 rounded-full ease-in-out hover:-translate-y-1.5 hover:font-bold duration-500"><Link href="/series">Series</Link></li>
-                    <li className="px-3 rounded-full ease-in-out hover:-translate-y-1.5 hover:font-bold duration-500"><Link href="/book">Book</Link></li>
-                    <li className="p-2 rounded-full ease-in-out hover:-translate-y-1.5 hover:font-bold duration-500"><Link href="/cart"><PiShoppingCart className="text-2xl"/></Link></li>
+                    <li className="px-3 rounded-full ease-in-out hover:-translate-y-1 hover:font-bold duration-500"><Link href="/store">Store</Link></li>
+                    <li className="px-3 rounded-full ease-in-out hover:-translate-y-1 hover:font-bold duration-500"><Link href="/series">Series</Link></li>
+                    <li className="px-3 rounded-full ease-in-out hover:-translate-y-1 hover:font-bold duration-500"><Link href="/book">Book</Link></li>
+                    <li className="p-2 rounded-full ease-in-out hover:-translate-y-1 hover:font-bold duration-500"><Link href="/cart"><PiShoppingCart className="text-2xl"/></Link></li>
                 </ul>
             </div>
         </nav>
